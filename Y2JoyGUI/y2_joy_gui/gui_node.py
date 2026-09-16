30d3076ac808d151bf9467df3d3011c1a1c94ddc
Jaeyun Sim <wodbs02221@gmail.com>
2026-09-15 21:50:55 -0400

    Disable Y2JoyGUI web cache

--- Y2JoyGUI/y2_joy_gui/gui_node.py
+++ Y2JoyGUI/y2_joy_gui/gui_node.py
@@ -62,6 +62,8 @@ class Y2JoyGuiNode(Node):
                 self.send_header("Access-Control-Allow-Origin", "*")
                 self.send_header("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
                 self.send_header("Access-Control-Allow-Headers", "Content-Type")
+                self.send_header("Cache-Control", "no-store, no-cache, must-revalidate, max-age=0")
+                self.send_header("Pragma", "no-cache")
                 super().end_headers()
 
             def do_OPTIONS(self):
